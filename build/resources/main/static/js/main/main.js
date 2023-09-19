f4a2e9292d6266b5c80b8cff35e585dab8b2077f
gusnl88 <gusnl88@naver.com>
2023-09-19 16:00:55 +0900

    댓글 수정

--- build/resources/main/static/js/main/main.js
+++ build/resources/main/static/js/main/main.js
@@ -2,6 +2,7 @@ const $toggleBtn = document.querySelector(".toggleBtn");
 const $menu = document.querySelector(".menu");
 const $logo = document.querySelector(".logo")
 
+
 $toggleBtn.addEventListener("click", () => {
 $menu.classList.toggle("active");
 $logo.classList.toggle("active");
@@ -28,32 +29,33 @@ document.addEventListener("DOMContentLoaded", () => {
             // 한글 문자만 포함되어 있는지 확인하는 정규 표현식
                 const isKorean = /^[가-힣]+$/.test(name);
 
-                if (!isKorean || name.length > 10) {
-                    alert("이름은 한글(10자이내) 이름만 가능합니다.");
-                    nameInput.value = ""; // 입력 내용 지우기
-                    return;
-                }
-
-            if (content.length > 150) {
-            alert("내용은 150자 이내로 등록 가능합니다.");
-            return;
+            if (!isKorean || name.length > 10) {
+                alert("이름은 한글(10자이내) 이름만 가능합니다.");
+                nameInput.value = ""; // 입력 내용 지우기
+                return;
             }
-        const response = await sendCommentToServer(name, content);
 
-        if (response.status === 200) {
-          // 댓글 등록에 성공하면 화면에 댓글을 추가합니다.
-//          const commentItem = document.createElement("li");
-//          commentItem.className = "list-group-item";
-//          commentItem.innerText = `${name}: ${content}`;
-//          commentList.appendChild(commentItem);
+            if (content.length > 20) {
+                alert("내용은 20자 이내로 등록 가능합니다.");
+                return;
+            }
+            const response = await sendCommentToServer(name, content);
 
-          // 입력 필드 초기화
-//          nameInput.value = "";
-//          contentInput.value = "";
-          location.reload(); //등록후 새로고침
-        } else {
-          console.error("서버에서 오류 응답을 받았습니다.");
-        }
+            if (response.status === 200) {
+
+                 const responseData = await response.text(); // JSON 대신 text로 응답을 받음
+                  if (responseData === "success") {
+                        location.reload(); //등록후 새로고침
+                        alert("글이 성공적으로 등록되었습니다.\n 글은 3번만 등록 가능합니다.");
+                  } else if (responseData === "exceeded") {
+                        location.reload(); //등록후 새로고침
+                        alert("글등록 횟수를 초과하였습니다.");
+                        return;
+                  }
+            } else {
+              console.error("서버에서 오류 응답을 받았습니다.");
+            }
+
       } catch (error) {
         console.error("댓글을 업데이트하는 동안 오류가 발생했습니다.", error);
       }
