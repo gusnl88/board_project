e623ab0d302cb35f43b423f40d4c285807312ec9
gusnl88 <gusnl88@naver.com>
2023-11-02 16:36:13 +0900

    swiper And css update

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -81,100 +81,100 @@ document.addEventListener("DOMContentLoaded", () => {
 // ********************************************************  이미지 슬라이드 함수
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const project1 = document.querySelector("#project1");
-    const project2 = document.querySelector("#project2");
-    const project3 = document.querySelector("#project3");
+//document.addEventListener("DOMContentLoaded", function () {
+//    const project1 = document.querySelector("#project1");
+//    const project2 = document.querySelector("#project2");
+//    const project3 = document.querySelector("#project3");
+//
+//    // 첫 번째 프로젝트 슬라이더 초기화
+//    setupSlider(project1);
+//
+//    // 두 번째 프로젝트 슬라이더 초기화
+//    setupSlider(project2);
+//    // 세 번째 프로젝트 슬라이더 초기화
+//    setupSlider(project3);
+//});
 
-    // 첫 번째 프로젝트 슬라이더 초기화
-    setupSlider(project1);
-
-    // 두 번째 프로젝트 슬라이더 초기화
-    setupSlider(project2);
-    // 세 번째 프로젝트 슬라이더 초기화
-    setupSlider(project3);
-});
-
- function setupSlider(sliderContainer) { //슬라이더 함수를 생성하여 요소를 매개변수로 받아 실행
-     const slider = sliderContainer.querySelector(".slider");
-     const prevButton = sliderContainer.querySelector(".prev");
-     const nextButton = sliderContainer.querySelector(".next");
-     const currentPageElement = sliderContainer.querySelector(".currentPage");
-     const totalPagesElement = sliderContainer.querySelector(".totalPages");
-
-     let imageWidth; // 이미지의 너비
-     let position = 0;
-     let currentPage = 1; // 현재 페이지
-     const totalPages = slider.children.length; // 총 페이지 수
-
-     // 함수를 사용하여 이미지 너비 설정
-     function setImageWidth() {
-         if (window.innerWidth < 768) {
-             // 모바일 화면
-             imageWidth = 275.35;
-         } else {
-             // 데스크톱 화면
-             imageWidth = 390.58;
-         }
-     }
-
-     // 초기 이미지 너비 설정
-     setImageWidth();
-
-     // 현재 페이지와 총 페이지 업데이트
-     function updatePageInfo() {
-         currentPageElement.textContent = currentPage;
-         totalPagesElement.textContent = totalPages;
-     }
-
-     // 다음 이미지로 이동
-     function nextSlide() {
-         if (position > -(imageWidth * (totalPages - 1))) {
-             position -= imageWidth;
-             slider.style.transform = `translateX(${position}px)`;
-             currentPage += 1;
-             updatePageInfo();
-         } else {
-             // 마지막 요소에서 다음 버튼 클릭 시 첫 번째 요소로 이동
-             position = 0;
-             slider.style.transform = `translateX(${position}px)`;
-             currentPage = 1;
-             updatePageInfo();
-         }
-     }
-
-     // 이전 이미지로 이동
-     function prevSlide() {
-         if (position < 0) {
-             position += imageWidth;
-             slider.style.transform = `translateX(${position}px)`;
-             currentPage -= 1;
-             updatePageInfo();
-         } else {
-             // 첫 번째 요소에서 이전 버튼 클릭 시 마지막 요소로 이동
-             position = -(imageWidth * (totalPages - 1));
-             slider.style.transform = `translateX(${position}px)`;
-             currentPage = totalPages;
-             updatePageInfo();
-         }
-     }
-
-     // 윈도우 크기가 변경될 때 이미지 너비 다시 설정
-     window.addEventListener("resize", setImageWidth);
-
-     // 이전 버튼 클릭 시
-     prevButton.addEventListener("click", function () {
-         prevSlide();
-     });
-
-     // 다음 버튼 클릭 시
-     nextButton.addEventListener("click", function () {
-         nextSlide();
-     });
-
-     // 초기 페이지 정보 업데이트
-     updatePageInfo();
- }
+// function setupSlider(sliderContainer) { //슬라이더 함수를 생성하여 요소를 매개변수로 받아 실행
+//     const slider = sliderContainer.querySelector(".slider");
+//     const prevButton = sliderContainer.querySelector(".prev");
+//     const nextButton = sliderContainer.querySelector(".next");
+//     const currentPageElement = sliderContainer.querySelector(".currentPage");
+//     const totalPagesElement = sliderContainer.querySelector(".totalPages");
+//
+//     let imageWidth; // 이미지의 너비
+//     let position = 0;
+//     let currentPage = 1; // 현재 페이지
+//     const totalPages = slider.children.length; // 총 페이지 수
+//
+//     // 함수를 사용하여 이미지 너비 설정
+//     function setImageWidth() {
+//         if (window.innerWidth < 768) {
+//             // 모바일 화면
+//             imageWidth = 275.35;
+//         } else {
+//             // 데스크톱 화면
+//             imageWidth = 390.58;
+//         }
+//     }
+//
+//     // 초기 이미지 너비 설정
+//     setImageWidth();
+//
+//     // 현재 페이지와 총 페이지 업데이트
+//     function updatePageInfo() {
+//         currentPageElement.textContent = currentPage;
+//         totalPagesElement.textContent = totalPages;
+//     }
+//
+//     // 다음 이미지로 이동
+//     function nextSlide() {
+//         if (position > -(imageWidth * (totalPages - 1))) {
+//             position -= imageWidth;
+//             slider.style.transform = `translateX(${position}px)`;
+//             currentPage += 1;
+//             updatePageInfo();
+//         } else {
+//             // 마지막 요소에서 다음 버튼 클릭 시 첫 번째 요소로 이동
+//             position = 0;
+//             slider.style.transform = `translateX(${position}px)`;
+//             currentPage = 1;
+//             updatePageInfo();
+//         }
+//     }
+//
+//     // 이전 이미지로 이동
+//     function prevSlide() {
+//         if (position < 0) {
+//             position += imageWidth;
+//             slider.style.transform = `translateX(${position}px)`;
+//             currentPage -= 1;
+//             updatePageInfo();
+//         } else {
+//             // 첫 번째 요소에서 이전 버튼 클릭 시 마지막 요소로 이동
+//             position = -(imageWidth * (totalPages - 1));
+//             slider.style.transform = `translateX(${position}px)`;
+//             currentPage = totalPages;
+//             updatePageInfo();
+//         }
+//     }
+//
+//     // 윈도우 크기가 변경될 때 이미지 너비 다시 설정
+//     window.addEventListener("resize", setImageWidth);
+//
+//     // 이전 버튼 클릭 시
+//     prevButton.addEventListener("click", function () {
+//         prevSlide();
+//     });
+//
+//     // 다음 버튼 클릭 시
+//     nextButton.addEventListener("click", function () {
+//         nextSlide();
+//     });
+//
+//     // 초기 페이지 정보 업데이트
+//     updatePageInfo();
+// }
 
 
 // ********************************************************  하단우측 버튼 활성화 코드
@@ -224,3 +224,24 @@ document.addEventListener("DOMContentLoaded", function () {
             modal.style.display = "none";
         }, 400); // 시간은 transition의 시간과 일치시킵니다.
     }
+
+ //**************************************************** 스와이퍼 슬라이드
+    // 슬라이더 초기화 함수
+ function initSwiper(containerId, paginationId) {
+     new Swiper(containerId, {
+         loop: true,
+         spaceBetween: 10,
+         pagination: {
+             el: paginationId,
+             clickable: true,
+         },
+         autoplay: {
+             delay: 3000,
+         },
+     });
+ }
+
+ // 각 슬라이더를 초기화
+ initSwiper('#swiper-container-1', '#swiper-pagination-1');
+ initSwiper('#swiper-container-2', '#swiper-pagination-2');
+ initSwiper('#swiper-container-3', '#swiper-pagination-3');
