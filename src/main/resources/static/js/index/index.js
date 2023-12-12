f435f7486afa3e40ebd361571c8552147ed51d7b
gusnl88 <gusnl88@naver.com>
2023-12-12 11:05:11 +0900

    index mousemove update2

--- src/main/resources/static/js/index/index.js
+++ src/main/resources/static/js/index/index.js
@@ -1,16 +1,17 @@
 document.addEventListener("mousemove", function (e) {
-    var followers = [
-        { id: "follower", offset: 15 },
-        { id: "follower2", offset: 25 },
-        { id: "follower3", offset: 35 }
-    ];
-
-    followers.forEach((follower, index) => {
-        setTimeout(() => {
-            var element = document.getElementById(follower.id);
-            element.style.display = "block";
-            element.style.left = e.pageX + follower.offset + "px";
-            element.style.top = e.pageY + follower.offset + "px";
-        }, 100 + 50 * index);
-    });
+    if (!('ontouchstart' in window) || (navigator.maxTouchPoints < 1) || (navigator.msMaxTouchPoints < 1)) {
+                var followers = [
+                    { id: "follower", offset: 15 },
+                    { id: "follower2", offset: 25 },
+                    { id: "follower3", offset: 35 }
+                ];
+            followers.forEach((follower, index) => {
+                setTimeout(() => {
+                    var element = document.getElementById(follower.id);
+                    element.style.display = "block";
+                    element.style.left = e.pageX + follower.offset + "px";
+                    element.style.top = e.pageY + follower.offset + "px";
+                }, 100 + 50 * index);
+            });
+        }
 });
